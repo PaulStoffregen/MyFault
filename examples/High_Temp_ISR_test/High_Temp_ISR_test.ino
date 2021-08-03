8f55f3f15df2fff005c42a421fbdca237bbf6b8a
Mike S <CyberMerln@gmail.com>
2021-08-02 20:36:40 -0400

    Fixed test sketches

--- examples/High_Temp_ISR_test/High_Temp_ISR_test.ino
+++ examples/High_Temp_ISR_test/High_Temp_ISR_test.ino
@@ -6,14 +6,14 @@ extern "C" uint32_t set_arm_clock(uint32_t frequency); // clockspeed.c
 static int16_t frequency = 0x03U;
 static int32_t highAlarmTemp   = 50U;
 static int32_t lowAlarmTemp    = 25U;
-static int32_t panicAlarmTemp  = 90U;
+static int32_t panicAlarmTemp  = 55U;
 volatile bool TempAlarm = false;
 
 void setup() {
   Serial.begin(9600);
   Serial.print(CrashReport);
   tempmon_setup();
-  //set_arm_clock(912000000);
+  set_arm_clock(912000000);
   delay(500);
   //Attach window for high/low window
   attachInterruptVector(IRQ_TEMPERATURE, &High_Low_Temp_isr);
@@ -25,7 +25,7 @@ void loop() {
   float temperature = tempmonGetTemp();
 
   if(tempPrint > 1000) {
-    Serial.printf("Temp(degC) = , %f", temperature);
+    Serial.printf("Temp(degC) = , %f\n", temperature);
     tempPrint = 0;
   }
 
@@ -54,8 +54,10 @@ void tempmon_setup(void)
 
   //first power on the temperature sensor - no register change 
   //power is already on so uncomment for example
-  TEMPMON_TEMPSENSE0 &= ~0x1U;
+  //TEMPMON_TEMPSENSE0 &= ~0x1U;
 
+  //Stop current temp monitoring
+  TEMPMON_TEMPSENSE0 &= ~0x2U;
   //set monitoring frequency - no register change
   TEMPMON_TEMPSENSE1 = (((uint32_t)(((uint32_t)(frequency)) << 0U)) & 0xFFFFU);
   
@@ -66,7 +68,7 @@ void tempmon_setup(void)
     roomCount = (uint32_t)(calibrationData & 0xFFF00000U) >> 0x14U;
     s_hot_ROOM = (float) (s_hotTemp) - 25.0f;
     s_roomC_hotC = (float) roomCount - (float) s_hotCount;
-  
+
     //time to set alarm temperatures
   //Set High Alarm Temp
   tempCodeVal = ((float)s_hotCount + ((float)s_hotTemp - highAlarmTemp) * s_roomC_hotC / s_hot_ROOM);
@@ -74,8 +76,11 @@ void tempmon_setup(void)
   
   //Set Panic Alarm Temp
   tempCodeVal = ((float)s_hotCount + ((float)s_hotTemp - panicAlarmTemp) * s_roomC_hotC / s_hot_ROOM);
-  TEMPMON_TEMPSENSE2 |= (((uint32_t)(((uint32_t)(tempCodeVal)) << 16U)) & 0xFFF0000U);
+    TEMPMON_TEMPSENSE2 |= (((uint32_t)(((uint32_t)(tempCodeVal)) << 16U)) & 0xFFF0000U);
   
+  // Set Low Temp Alarm Temp
+  tempCodeVal = ((float)s_hotCount + ((float)s_hotTemp - lowAlarmTemp) * s_roomC_hotC / s_hot_ROOM);
+    TEMPMON_TEMPSENSE2 |= (((uint32_t)(((uint32_t)(tempCodeVal)) << 0U)) & 0xFFFU);
   // Set Low Temp Alarm Temp
   tempCodeVal = ((float)s_hotCount + ((float)s_hotTemp - lowAlarmTemp) * s_roomC_hotC / s_hot_ROOM);
     TEMPMON_TEMPSENSE2 |= (((uint32_t)(((uint32_t)(tempCodeVal)) << 0U)) & 0xFFFU);
